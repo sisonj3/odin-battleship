23c6bf225577e0bf62fbdb1db9b17e3283c80062
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-11-11 16:49:58 -0800

    Re-compiled program

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
+import setupGame from './game-setup';
+import './style.css';
+
 console.log("-----Start-----");
+
+// DOM Elements
+const overlay = document.querySelector('.overlay');
+const shipSetupDiv = document.querySelector('.ship-setup');
+const closeBtn = document.querySelector('.delete');
+
+const newGame = document.querySelector('.new-game-btn');
+
+newGame.addEventListener('click', setupGame);
+
+closeBtn.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+    shipSetupDiv.classList.add('hidden');
+});
+
 console.log("------End------");
