5012fd00ed1c08627ef47a38cd57814b72245953
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-11-11 16:48:53 -0800

    Added function to let the cpu place ships on its board

--- src/cpu.js
+++ src/cpu.js
@@ -1,10 +1,54 @@
 const player = require('./player');
+const ship = require('./ship');
 
 // Cpu Factory
 function cpu(isTurn) {
 
     const cpuPlayer = player(isTurn);
     let possibleAttacks = generateAttacks();
+
+    const generateBoard = (() => {
+        let ships = [ship(5), ship(4), ship(3), ship(3), ship(2)];
+        let n = 0;
+
+        // Possible Postions
+        let possiblePositions = generateAttacks();
+
+        while (n < ships.length) {
+
+            // Booleans
+            let alignment = Math.random() < 0.5;
+
+            // Coordinates
+            let z = Math.floor(Math.random() * (possiblePositions.length - 1));
+            let position = possiblePositions[z];
+            let x = position[0];
+            let y = position[1];
+
+            // If ship is valid
+            if (cpuPlayer.playerBoard.placeShip(ships[n], x, y, alignment)) {
+                possiblePositions = generateAttacks();
+                n++;
+            } else {
+                possiblePositions = possiblePositions.filter(function (e) { return e != position; });
+            }
+            
+        }
+
+    })();
+
+    function generatePositions() {
+        let positions = [];
+
+        for (let i = 0; i < cpuPlayer.playerBoard.length; i++){
+            positions.push([])
+            for (let j = 0; j < cpuPlayer.playerBoard.length; j++){
+                positions[i].push([i, j]);
+            }
+        }
+
+        return positions;
+    }
 
     function generateAttacks() {
         let attacks = [];
